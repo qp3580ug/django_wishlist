c38b81b2e9aa9aa8945a2421c2fccba654a8bddb
Nathan W <nathan@email.com>
2019-10-10 20:45:45 -0500

    Added final functional test from slides

--- travel_wishlist/functional_tests.py
+++ travel_wishlist/functional_tests.py
@@ -25,3 +25,28 @@ class TitleTest(LiveServerTestCase):
         self.selenium.get(self.live_server_url)
         assert 'Travel Wishlist' in self.selenium.title
 
+class AddEditPlacesTests(LiveServerTestCase):
+
+    fixtures = ['test_places']
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()  
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_add_new_place(self):
+
+        self.browser.get(self.live_server_url)
+        input_name = self.browser.find_element_by_id('id_name')
+        input_name.send_keys('Denver') 
+        add_button = self.browser.find_element_by_id('add-new-place')
+        add_button.click()
+
+        wait_for_denver = self.browser.find_element_by_id('place-name-5')
+
+        assert 'Tokyo' in self.browser.page_source
+        assert 'New York' in self.browser.page_source
+
+        assert 'Denver' in self.browser.page_source
