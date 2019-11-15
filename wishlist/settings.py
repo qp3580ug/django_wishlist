18d58a69fa782fa7bff21be47c23d1189e817824
Nathan Walter <qp3580ug@go.minnstate,edu>
2019-11-14 20:50:14 -0600

    Code for cloud server

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wishlist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'places',
+        'USER': 'traveller',
+        'PASSWORD': os.getenv('traveller_PW'),
+        'HOST': '/cloudsql/resolute-vault-259101:us-central1:wishlist-db',
+        'PORT': '5432'
     }
 }
 
